44087fa3367726fb5c6173c79602f8fd6e61db82
Georgy Samsonidze <Georgy.Samsonidze@us.bosch.com>
2020-02-09 11:09:28 -0800

    Updated BoltzTraP2 patch and examples from v18.5.1 to current version (trunk).

--- test/silicon2/silicon_crt.py
+++ test/silicon2/silicon_crt.py
@@ -3,7 +3,7 @@ import numpy as np
 from BoltzTraP2 import dft
 from BoltzTraP2 import sphere
 from BoltzTraP2 import fite
-from BoltzTraP2 import bandlib as BL
+from BoltzTraP2 import bandlib
 from BoltzTraP2 import units
 
 dirname = './'
@@ -17,15 +17,15 @@ ecut, efcut, deltae, tmax, deltat, lpfac = 1.0 * RYDBERG, 0.3 * RYDBERG, 0.0005 
 # Load the input
 data = dft.DFTData(dirname)
 # Select the interesting bands
-nemin, nemax = data.bandana(emin=data.fermi - ecut, emax=data.fermi + ecut)
+data.bandana(emin=data.fermi - ecut, emax=data.fermi + ecut)
 # Set up a k point grid with roughly five times the density of the input
-equivalences = sphere.get_equivalences(data.atoms, len(data.kpoints) * lpfac)
+equivalences = sphere.get_equivalences(data.atoms, data.magmom, len(data.kpoints) * lpfac)
 # Perform the interpolation
 coeffs = fite.fitde3D(data, equivalences)
 
 lattvec = data.get_lattvec()
 eband, vvband, cband = fite.getBTPbands(equivalences, coeffs, lattvec)
-epsilon, dos, vvdos, cdos = BL.BTPDOS(eband, vvband, erange=[data.fermi - ecut, data.fermi + ecut], npts=round(2 * ecut / deltae), scattering_model='uniform_tau')
+epsilon, dos, vvdos, cdos = bandlib.BTPDOS(eband, vvband, erange=[data.fermi - ecut, data.fermi + ecut], npts=round(2 * ecut / deltae), scattering_model='uniform_tau')
 
 # Define the temperatures and chemical potentials we are interested in
 #Tr = np.arange(deltat, tmax + deltat / 2, deltat)
@@ -36,7 +36,7 @@ Tr = np.arange(deltat, tmax + deltat / 2, deltat)
 mur = np.empty_like(Tr)
 _nelect = data.nelect
 for iT, T in enumerate(Tr):
-    mur[iT] = BL.solve_for_mu(epsilon, dos, _nelect - doping_level, T, data.dosweight, refine=True)
+    mur[iT] = bandlib.solve_for_mu(epsilon, dos, _nelect - doping_level, T, data.dosweight, refine=True)
 
     # The flow from this point is again similar to that in parse_integrate,
     # with the exception that the chemical potentials are different for
@@ -47,7 +47,7 @@ L0, L1, L2 = (np.empty((Tr.shape[0], 3, 3)) for ii in range(3))
 Lm11 = np.empty((Tr.shape[0], 3, 3, 3))
 # Obtain the Fermi integrals required to get the Onsager coefficients
 for iT, T in enumerate(Tr):
-    (N[iT], L0[iT], L1[iT], L2[iT], Lm11[iT]) = BL.fermiintegrals(epsilon, dos, vvdos, mur=np.array([mur[iT]]), Tr=np.array([T]), dosweight=data.dosweight, cdos=cdos)
+    (N[iT], L0[iT], L1[iT], L2[iT], Lm11[iT]) = bandlib.fermiintegrals(epsilon, dos, vvdos, mur=np.array([mur[iT]]), Tr=np.array([T]), dosweight=data.dosweight, cdos=cdos)
 #N += data.nelect # incorrect because of missing states due to ecut and efcut
 # ??? N -= N[0, round(N.shape[1] / 2)] # zero doping at low T in the middle of the band gap
 volume = data.get_volume()
@@ -55,7 +55,7 @@ volume = data.get_volume()
 sigma, seebeck, kappa = (np.empty((Tr.shape[0], 3, 3)) for ii in range(3))
 Hall = np.empty((Tr.shape[0], 3, 3, 3))
 for iT, T in enumerate(Tr):
-    (sigma[iT], seebeck[iT], kappa[iT], Hall[iT]) = BL.calc_Onsager_coefficients(np.array([[L0[iT]]]), np.array([[L1[iT]]]), np.array([[L2[iT]]]), np.array([mur[iT]]), np.array([T]), volume, np.array([[Lm11[iT]]]))
+    (sigma[iT], seebeck[iT], kappa[iT], Hall[iT]) = bandlib.calc_Onsager_coefficients(np.array([[L0[iT]]]), np.array([[L1[iT]]]), np.array([[L2[iT]]]), np.array([mur[iT]]), np.array([T]), volume, np.array([[Lm11[iT]]]))
 
 # Rescale the carrier count into a volumetric density in cm^-3
 #N = -N / (volume / (units.Meter / 100) ** 3)
