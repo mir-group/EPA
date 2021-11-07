f11cfdbaff5ecb49e5e77788c35073d8dda34477
Georgy Samsonidze <Georgy.Samsonidze@us.bosch.com>
2021-11-07 05:22:25 -0800

    Documentation on Fermi level

--- test/silicon/qe2boltz.py
+++ test/silicon/qe2boltz.py
@@ -7,10 +7,15 @@ f2 = ['{0:s}.def'.format(name), '{0:s}.intrans'.format(name), '{0:s}.struct'.for
 bnd = brave.Energy()
 bnd.read('pw-out', f1)
 
+# For insulators, set the Fermi level in the middle of the band gap !!!! DO NOT TRY THIS FOR METALS !!!!
 evbm, ecbm, kvbm, kcbm = bnd.calc_efermi()
 egap = ecbm - evbm
 ss = '{0:s}  nelec = {1:.2f}  evbm = {2:.6f}  ecbm = {3:.6f}  egap = {4:.6f}  efermi = {5:.6f}  kvbm = {6[0]:.3f} {6[1]:.3f} {6[2]:.3f}  kcbm = {7[0]:.3f} {7[1]:.3f} {7[2]:.3f}'.format(
     name, bnd.nelec, evbm, ecbm, egap, bnd.efermi, kvbm, kcbm)
 print(ss)
+
+# For metals, don't mess with the Fermi level
+#ss = '{0:s}  nelec = {1:.2f}  efermi = {2:.6f}'.format(name, bnd.nelec, bnd.efermi))
+#print(ss)
 
 bnd.write('boltztrap-in', f2)
